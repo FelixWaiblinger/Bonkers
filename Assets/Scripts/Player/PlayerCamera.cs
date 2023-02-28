6327dc1dbdd3d42707c5822fad8cc0a537808db5
Felix Waiblinger <diebreze@gmail.com>
2023-02-28 01:14:43 +0100

    added cinemachine camera

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -1,28 +1,37 @@
 using UnityEngine;
 using Unity.Netcode;
+using Cinemachine;
 
 public class PlayerCamera : NetworkBehaviour
 {
-    [SerializeField] private Vector3 _offset = new Vector3(0, 10, -8);
-    [SerializeField] private float _rotationX = 60;
+    // [SerializeField] private Vector3 _offset = new Vector3(0, 10, -8);
+    // [SerializeField] private float _rotationX = 60;
 
-    private Transform _playerCamera;
+    private CinemachineVirtualCamera _playerCamera;
 
     void Start()
     {
         Debug.Log("Grab camera");
-        if (IsOwner) _playerCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        if (IsOwner)
+        {
+            _playerCamera =
+                GameObject.FindGameObjectWithTag("PlayerCamera")
+                          .GetComponent<CinemachineVirtualCamera>();
+
+            _playerCamera.Follow = transform;
+            _playerCamera.LookAt = transform;
+        }
     }
 
-    void Update()
-    {
-        if (!IsOwner) return;
+    // void Update()
+    // {
+    //     if (!IsOwner) return;
+    // 
+    //     _playerCamera.position = Vector3.zero + transform.localPosition + _offset;
+    // }
 
-        _playerCamera.position = Vector3.zero + transform.localPosition + _offset;
-    }
-
-    private void OnApplicationFocus(bool focusStatus) {
-        if (focusStatus && _playerCamera != null)
-            _playerCamera.rotation = Quaternion.Euler(_rotationX, 0, 0);
-    }
+    // private void OnApplicationFocus(bool focusStatus) {
+    //     if (focusStatus && _playerCamera != null)
+    //         _playerCamera.rotation = Quaternion.Euler(_rotationX, 0, 0);
+    // }
 }
--- README.md
+++ README.md
@@ -3,11 +3,9 @@
 Bonkers is a 3D Online Multiplayer Arena Deathmatch / Battle Royal game.
 
 ## TODO
-- Player camera (Battlerite Camera mit optionalem FIXPUNKT (Immer selber winkel))
 - Abilities (UI and choices similar to skill tree)
 - Win condition
 - Teams
-- Projectile obstacle collisions
 - Melee attacks
 - Support player type
 - Create first map
